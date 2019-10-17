94b5af5f55cb144269a0eff0bdf581a2e51a04cb
Dylan Vanhout <190160@virtualwindow.co.za>
2019-10-17 19:31:56 +0200

    Add js code for API
    Testing the Api.

--- js/watchlist.js
+++ js/watchlist.js
@@ -4,5 +4,35 @@
 
 
 
+//Code example from the tutorial in class.
+$(function(){
+
+    var apiRequest = new XMLHttpRequest();
+
+    apiRequest.open('GET', "https://api.themoviedb.org/3/movie/550?api_key=3b877e38df9daff06def35c184995a4e", true);
+
+    apiRequest.send();
+
+    console.log(apiRequest);
+
+
+})
+
+//Code example from the developers guide on the website....
+var data = "{}";
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("GET", "https://api.themoviedb.org/3/movie/550?api_key=3b877e38df9daff06def35c184995a4e");
+
+xhr.send(data);
+
 
 
