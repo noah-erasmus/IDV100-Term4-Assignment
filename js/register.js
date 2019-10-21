8af6ffbeee7c5a1ffb97d0db1e95b6c95d324987
Dylan Vanhout <190160@virtualwindow.co.za>
2019-10-21 16:28:33 +0200

    Update html, css, js for registraion and login

--- js/register.js
+++ js/register.js
@@ -1,3 +1,41 @@
 //Javascript for the login and signup page/s goes here.
 //DO NOT TOUCH :)
 
+
+
+//Document Ready!
+// $(function(){
+// // Toggle Display Sing-In / Registration Divs
+
+
+
+
+
+// });
+
+// var array = [1, 2, 3];
+
+// localStorage.setItem("array", JSON.stringify(array));
+// array = JSON.parse(localStorage.getItem("array"));
+
+
+// console.log(typeof array); //object
+// console.log(array); //[1, 2, 3]
+
+
+
+// On button click toggle between login and registration screens
+$(function(){
+    $('#toggler-button').on('click', function(e){
+        $('.login-content').css('display', 'none');
+        $('.register-content').css('display', 'inline');
+        e.preventDefault();
+    });
+});
+$(function () {
+    $('#toggler-button2').on('click', function (e) {
+    $('.register-content').css('display', 'none');
+    $('.login-content').css('display', 'inline');
+    e.preventDefault();
+});
+});
