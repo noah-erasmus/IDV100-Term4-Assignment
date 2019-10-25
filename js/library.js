2cc0cc29032cf8cf5945869a3559c0c44c9131fe
noah-erasmus <noah.erasmus@gmail.com>
2019-10-25 11:07:09 +0200

    Finish poster functionality and fix genres problem

--- js/library.js
+++ js/library.js
@@ -4,14 +4,15 @@ $(function(){
     apiRequest.send();
     apiRequest.onload = function(){
         var data = JSON.parse(this.response);
+        var base_url =  "https://image.tmdb.org/t/p/original";
         function createEntries(poster_path, original_title, vote_average, genre_ids, release_date){
-            $(".entry-line").append("<div class='entry col-xs-2 col-sm-2 col-md-2 col-lg-2'><div class='entry-poster'><img src=" + poster_path + "></div><div class='entry-details'><div class='details-left'><p class='entry-title'>" + original_title + "</p><p class='entry-rating'>" + vote_average + "</p></div><div class='details-right'><p class='entry-genre'>" + genre_ids + "</p><p class='entry-release'>" + release_date + "</p></div></div><div class='entry-btns'><a href='#'><div class='read-more-btn'><p>Read <br>more</p></div></a><div class='add-watchlist-btn'><p>Add to <br>watchlist</p></div></div></div");
+            $(".entry-line").append("<div class='entry col-xs-2 col-sm-2 col-md-2 col-lg-2'><div class='entry-poster'><img src='" + poster_path + "'></div><div class='entry-details'><div class='details-left'><p class='entry-title'>" + original_title + "</p><p class='entry-rating'>" + vote_average + "</p></div><div class='details-right'><p class='entry-genre'>" + genre_ids + "</p><p class='entry-release'>" + release_date + "</p></div></div><div class='entry-btns'><a href='#'><div class='read-more-btn'><p>Read <br>more</p></div></a><div class='add-watchlist-btn'><p>Add to <br>watchlist</p></div></div></div");
         }
         for(i = 0; i < data.results.length; i++){
-            poster_path = data.results[i].poster_path;
+            poster_path = base_url + data.results[i].poster_path;
             original_title = data.results[i].original_title;
             vote_average = data.results[i].vote_average;
-            genre_ids = data.results[i].genre_ids[0];
+            genre_ids = window.genre_obj[data.results[i].genre_ids[0]];
             release_date = data.results[i].release_date;
             
             createEntries(poster_path, original_title, vote_average, genre_ids, release_date);
