e7e064129f4e53fc2a45f13566c5185c9560bebc
Noah <noah.erasmus@gmail.com>
2019-10-28 11:23:34 +0200

    Update library.js

--- js/library.js
+++ js/library.js
@@ -17,5 +17,10 @@ $(function(){
             
             createEntries(poster_path, original_title, vote_average, genre_ids, release_date);
         }
+        $(".add-watchlist-btn").on("click",function(){
+            var storageReference = $(this).closest().find(".entry-title").text();
+            console.log(storageReference);
+            debugger;
+        })
     }
 })
