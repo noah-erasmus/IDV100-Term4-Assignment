3f39623d46c8ca41db03967cfb0ca86f52602118
Dylan Vanhout <190160@virtualwindow.co.za>
2019-10-28 16:18:14 +0200

    Change made to overall styling and javascript

--- js/register.js
+++ js/register.js
@@ -29,9 +29,31 @@ $(function () {
 if(window.localStorage){
     // console.log("Local Storage Supported"); //Yes
 
-    //Store the values using key-pair.
-    localStorage.setItem("user", "dylan");
-    localStorage.setItem("pass", "$777");
+    // //Store the values using key-pair.
+    // localStorage.setItem("user", "mike");
+    // localStorage.setItem("pass", "dev11");
+
+    //Trying to set an array of multiple users
+
+    var users = [
+        {
+            'id': 1,
+            'user': 'mike',
+            'pass': 'dev11'
+        },
+        {
+            'id': 2,
+            'user': 'bob',
+            'pass': 'dev22'
+        },
+        {
+            'id': 3,
+            'user': 'sam',
+            'pass': 'dev33'
+        }
+    ];
+    localStorage.setItem("users", JSON.stringify(users));
+    console.log(localStorage);
 
     //GET ITEM METHOD - Get data of local storage
     let user = localStorage.getItem("user");
@@ -43,6 +65,7 @@ if(window.localStorage){
         if(user == txtuser.value && pass == txtpass.value){
             message.innerHTML = "Login Successful!";
             setTimeout(function () {window.location.href = "../index.html";}, 1000);// Redirect to index page after successful login
+            
         }else{
             message.innerHTML = "Username or Password is Invalid!";//Warning Message
 
@@ -57,3 +80,43 @@ if(window.localStorage){
 // 1. on login change the user name to show on top right corner depending on which user is logged in. - localstorage user name.
 // 2. on login the user name must change and the profile picture accordingly.
 
+
+
+
+
+
+// var users = [
+//     { username: 'user1', password: 'pass1' },
+//     { username: 'user2', password: 'pass2' },
+//     { username: 'user3', password: 'pass3' }
+// ];
+
+// var button = document.getElementById('login');
+
+// button.onclick = function () {
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+
+//     for (var i = 0; i < users.length; i++) {
+//         if (username == users[i].username && password == users[i].password) {
+//             window.location.href = 'http://where/you/want/to/redirect/';
+//             break;
+//         } else {
+//             alert('You are trying to break in!');
+//         }
+//     }
+// }
+
+{/* <form>
+    <legend>Log In</legend>
+
+    <fieldset>
+        <label for="username">Username: </label>
+        <input id="username" type="text">
+
+            <label for="password">Password: </label>
+            <input id="password" type="password">
+
+                <button id="login" type="button">Log In!</button>
+   </fieldset>
+</form> */}
